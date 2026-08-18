93f421cd9a5717d0623cc3acb03f603b9f28c9ee
unl1kely <yacine.zobiri@gmail.com>
2026-08-18 15:32:53 +0100

    unicafe step2
    Added stats: all (sum), average, positive

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,15 +7,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
   const handleGood = ()=>{
-    setGood(good+1)
+    const updatedGood = good+1
+    setGood(updatedGood)
+    const updatedSum = all+1
+    setAll(updatedSum)
+    const updatedAverage = (updatedGood-bad)/updatedSum
+    setAverage(updatedAverage)
+    setPositive(updatedGood/updatedSum*100)
   }
   const handleNeutral = ()=>{
-    setNeutral(neutral+1)
+    const updatedNeutral = neutral+1
+    setNeutral(updatedNeutral)
+    const updatedSum = all+1
+    setAll(updatedSum)
+    const updatedAverage = (good-bad)/updatedSum
+    setAverage(updatedAverage)
+    setPositive(good/updatedSum*100)
   }
   const handleBad = ()=>{
-    setBad(bad+1)
+    const updatedBad = bad+1
+    setBad(updatedBad)
+    const updatedSum = all+1
+    setAll(updatedSum)
+    const updatedAverage = (good-updatedBad)/updatedSum
+    setAverage(updatedAverage)
+    setPositive(good/updatedSum*100)
   }
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -28,7 +51,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
